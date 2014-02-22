ac825254c2d17d8ee2df6be32c09957900619663
Szu-Kai Hsu (brucehsu) <brucehsu@gmail.com>
2014-02-22 16:57:59 +0800

    自動讀取OSX官版嘸蝦米的加字加詞內容並轉換，不需使用者手動輸出

--- liubox2rime.sh
+++ liubox2rime.sh
@@ -2,9 +2,22 @@
 #
 # liubox2rime.sh
 #
-# 將OSX版嘸蝦米輸出的liu.box轉換為RIME辭典格式，並寫入於boshiamy_[t,j].dict.yaml最尾端
+# 讀取OSX官版嘸蝦米的加字加詞內容並轉換為RIME詞庫格式，再寫入至相對應詞庫之最尾端
 #
 
-if [[ -f "liu.box" ]]; then
-    awk 'BEGIN{ORS=""} NR>1 && $1!="%chardef" {for (i=2; i<=NF; i++) print $i; print "\t" $1 "\n"}' < liu.box >> $1
-fi
+RIME_DICT="boshiamy_$1.dict.yaml"
+LIUBOX_PATH="$HOME/Library/Application Support/Boshiamy"
+LIUBOX_FILE="UserPhrasesTC.cin" # 預設使用正體中文版的詞庫
+
+case $1 in
+    "t")
+        LIUBOX_FILE="UserPhrasesTC.cin"
+        ;;
+    "j")
+        LIUBOX_FILE="UserPhrasesJP.cin"
+        ;;
+    *)
+        ;;
+esac
+
+awk 'BEGIN{ORS=""} NR>1 && $1!="%chardef" {for (i=2; i<=NF; i++) print $i; print "\t" $1 "\n"}' < "$LIUBOX_PATH/$LIUBOX_FILE" >> $RIME_DICT
